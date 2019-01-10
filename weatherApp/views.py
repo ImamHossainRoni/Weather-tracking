ceb5ff1926619c01c1390ac80ab3ae73a25510f7
imamhossainroni <imamhossainroni95@gmail.com>
2019-01-10 21:44:05 +0600

    Working with django forms

--- weatherApp/views.py
+++ weatherApp/views.py
@@ -1,20 +1,33 @@
 import requests
 from django.shortcuts import render,HttpResponse
-
+from .models import City
+from .forms import CityForm
 # Create your views here.
 def index(request):
     url ='http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=c99a630bdb6897a61e035784ec774d76'
-    city = 'Dhaka'
-    r = requests.get(url.format(city)).json()
-    city_weather = {
-        'city': city,
-        'temperature': r['main']['temp'],
-        'description':r['weather'][0]['description'],
-        'icon':r['weather'][0]['icon'],
+    
+    if request.method == 'POST':
+        pass
+    form = CityForm()
+    cities = City.objects.all()
+    weather_data = []
+    for city in cities:
 
-    }
+        r = requests.get(url.format(city)).json()
+        city_weather = {
+            'city': city.name,
+            'temperature': r['main']['temp'],
+            'description':r['weather'][0]['description'],
+            'icon':r['weather'][0]['icon'],
+
+        }
+        weather_data.append(city_weather)
+        print(weather_data)
+
     context = {
-        'city_weather': city_weather
-    }
+        'weather_data': weather_data,
+        'form': form,
+        }
+
     print(city_weather)
     return render(request,'index.html',context)
